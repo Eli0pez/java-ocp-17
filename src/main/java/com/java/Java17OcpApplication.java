ea3fb650f479439f0d9a946156aace8ec11442d1
David Hernandez <eliopez@192.168.0.25>
2024-02-22 19:43:43 -0500

    Initial Commit

---
+++ src/main/java/com/java/Java17OcpApplication.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java17OcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java17OcpApplication.class, args);
+	}
+
+}
